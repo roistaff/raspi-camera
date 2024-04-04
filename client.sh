22225cfbaa0e0c778490cec84963f5a782cd00ce
roistaff <roistaff1983@gmail.com>
2024-04-04 13:17:37 +0900

    fix bug hostname2

--- client.sh
+++ client.sh
@@ -1,3 +1,6 @@
-hostname=$(hostname)
+hostname=$1
+if [ "$hostname" = "" ]; then
+	hostname="raspberrypi.local"
+fi
 blob_data=$(curl "$hostname":8000)
 echo -n "$blob_data" | xxd -r -p > images/output.jpg
